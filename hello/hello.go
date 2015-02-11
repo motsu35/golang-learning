9e106d5cf865fecd0d6753f7c00e119e338ac799
motsu <motsu35@gmail.com>
2015-02-11 13:57:02 -0500

    finished hello world stuff. moving onto tour

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/motsu35/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
---
+++ stringutil/reverse.go
@@ -0,0 +1,11 @@
+//package stringutil contains utility string functions.
+package stringutil
+
+//reverse returns its argument string reversed
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
